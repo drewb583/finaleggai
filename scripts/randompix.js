a78caf424943e434d2bead9946849d6468a32f5a
Your Name <hyperson.andrew@gmail.com>
2024-02-09 02:05:55 -0500

    Update randompix.js

--- scripts/randompix.js
+++ scripts/randompix.js
@@ -1,7 +1,32 @@
-window.onload = choosePic;
+const imgEl = document.getElementById('random-image');
 
-var myPix = new Array("images/eggman1.gif","images/eggman2.gif", "images/eggman3.gif");
+const srcArray = ["images/eggman1.gif","images/eggman2.gif", "images/eggman3.gif"];
 
-function choosePic() {
-     var randomNum = Math.floor(Math.random() * myPix.length);
-     document.getElementById("myPicture").src = myPix[randomNum];
+imgEl.addEventListener('click', () => {
+    window.location.reload();
+    // Refreshes page when image clicked
+});
+
+randomImage();
+
+function randomImage() {
+
+    const randomIndex = Math.floor(Math.random()*srcArray.length);
+
+    if (localStorage.getItem('currentIndex') !== String(randomIndex)) {
+    // Checks if current index in local storage is NOT equal to new randm value (as string)...
+        
+        imgEl.src = srcArray[randomIndex];
+        //...if so, set random value as new image src...
+
+        localStorage.setItem('currentIndex', randomIndex);
+        //...and save as current index value in local storage
+
+    } else {
+      // random value and current index value match...
+
+        randomImage();
+        // ... so call random image function again
+
+    }
+}
