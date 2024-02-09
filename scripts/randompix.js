9bb79df0f1bf9a77998042672fa15560607673e9
Your Name <hyperson.andrew@gmail.com>
2024-02-09 02:38:42 -0500

    a

--- scripts/randompix.js
+++ scripts/randompix.js
@@ -1,32 +1,34 @@
-const imgEl = document.getElementById('random-image');
+// This listens for when the DOM (page) content gets loaded, and then calls for a random image
+document.addEventListener('DOMContentLoaded', (event) => {
+	let imgEl = document.getElementById("image");
+	const srcArray = ["images/eggman1.gif", "images/eggman2.gif", "images/eggman3.gif"];
+	
+	// Refreshes page when image clicked
+	imgEl.addEventListener('click', () => {
+		window.location.reload();
+	});
+	
+	function randomImage() {
+		const randomIndex = Math.floor(Math.random()*srcArray.length);
 
-const srcArray = ["images/eggman1.gif","images/eggman2.gif", "images/eggman3.gif"];
+		if (localStorage.getItem('currentIndex') !== String(randomIndex)) {
+		// Checks if current index in local storage is NOT equal to new randm value (as string)...
+			
+			imgEl.src = srcArray[randomIndex];
+			//...if so, set random value as new image src...
 
-imgEl.addEventListener('click', () => {
-    window.location.reload();
-    // Refreshes page when image clicked
+			localStorage.setItem('currentIndex', randomIndex);
+			//...and save as current index value in local storage
+
+		} else {
+		  // random value and current index value match...
+
+			randomImage();
+			// ... so call random image function again
+
+		}
+	}
+
+	// Now that everything is defined, call for a random image
+	randomImage();
 });
-
-randomImage();
-
-function randomImage() {
-
-    const randomIndex = Math.floor(Math.random()*srcArray.length);
-
-    if (localStorage.getItem('currentIndex') !== String(randomIndex)) {
-    // Checks if current index in local storage is NOT equal to new randm value (as string)...
-        
-        imgEl.src = srcArray[randomIndex];
-        //...if so, set random value as new image src...
-
-        localStorage.setItem('currentIndex', randomIndex);
-        //...and save as current index value in local storage
-
-    } else {
-      // random value and current index value match...
-
-        randomImage();
-        // ... so call random image function again
-
-    }
-}
